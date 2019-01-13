51d16cfcdfe4b524da0f2a7123b140c53b0c53f1
Sam Bahta <smabahta37@gmail.com>
2019-01-13 15:15:02 -0600

    banner now sticks to top of screen when scrolling

--- client.js
+++ client.js
@@ -1,5 +1,21 @@
 console.log('js');
-$(document).ready(function (){
+
+$(document).ready(function () {
   console.log('jq');
-  
-})
+
+  //makes banner stick when scrolled to top of window
+  const banner = $("#fixer");
+  const sticky = banner.offset().top;
+
+  window.onscroll = function () {
+    bannerFix();
+  };
+  function bannerFix() {
+    if (window.pageYOffset >= sticky) {
+      banner.addClass("sticky")
+    } else {
+      banner.removeClass("sticky")
+    }
+  };//end banner stick
+
+});
